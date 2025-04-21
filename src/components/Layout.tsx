12d2c79391f7dc497ef56176fa3edf4607476fb1
ViperTips <youngtips23@gmail.com>
2025-04-21 12:20:30 +0200

    added layout fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,18 @@
-
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout: React.FC = () => {
-  const location = useLocation();
-
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-      <Footer />
+      <div className="flex-1 flex flex-col">
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
